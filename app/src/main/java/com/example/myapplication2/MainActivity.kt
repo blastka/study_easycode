18e4ffaae3286f072f2d28aff362d44f62a6cbdb
blastka505 <necturna505@gmail.com>
2022-08-07 18:25:01 +0300

    изящное решение для textWatcher в случае если вы не ставите програмно текст

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
@@ -16,32 +17,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textInputEditText: TextInputEditText
 
-    private val textWatcher: TextWatcher = object : SimpleTextWatcher(){
-        override fun afterTextChanged(p0: Editable?) {
-            val input = p0.toString()
-            if(input.endsWith("@g")){
-                val fullMail = "${input}mail.com"
-                setText(fullMail)
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textInput = findViewById<TextInputLayout>(R.id.textInputLayout)
         textInputEditText = textInput.editText as TextInputEditText
-        textInputEditText.addTextChangedListener(textWatcher)
+        textInputEditText.listenChanges { text -> Log.e("MY", text) }
     }
 
-    private fun setText(text: String){
-        textInputEditText.removeTextChangedListener(textWatcher)
-        textInputEditText.setTextCorrectly(text)
-        textInputEditText.addTextChangedListener(textWatcher)
+    private fun TextInputEditText.listenChanges(block:(text: String) -> Unit){
+        addTextChangedListener(object : SimpleTextWatcher(){
+            override fun afterTextChanged(p0: Editable?) {
+                block.invoke(p0.toString())
+            }
+        })
     }
 
-    fun TextInputEditText.setTextCorrectly(text: CharSequence){
-        setText(text)
-        setSelection(text.length)
-    }
 }
