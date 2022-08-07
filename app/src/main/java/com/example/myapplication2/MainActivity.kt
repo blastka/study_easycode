2cc61ba2e70d9d489853de7677e366761130d27d
blastka505 <necturna505@gmail.com>
2022-08-07 18:17:03 +0300

    Создаем багованный заполнитель @g до @gmail.com

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -14,24 +14,34 @@ import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private companion object{
-        const val URL = "https://zavistnik.com/wp-content/uploads/2020/03/Android-kursy-zastavka.jpg"
+    private lateinit var textInputEditText: TextInputEditText
+
+    private val textWatcher: TextWatcher = object : SimpleTextWatcher(){
+        override fun afterTextChanged(p0: Editable?) {
+            val input = p0.toString()
+            if(input.endsWith("@g")){
+                val fullMail = "${input}mail.com"
+                setText(fullMail)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textInput = findViewById<TextInputLayout>(R.id.textInputLayout)
-        val textEdit = textInput.editText as TextInputEditText
+        textInputEditText = textInput.editText as TextInputEditText
+        textInputEditText.addTextChangedListener(textWatcher)
+    }
 
-        textEdit.addTextChangedListener(object : SimpleTextWatcher() {
-            override fun afterTextChanged(p0: Editable?) {
-                val input = p0.toString()
-                if(input.endsWith("@g")){
-                    val fullMail = "${input}mail.com"
-                    textEdit.setText(fullMail)
-                }
-            }
-        })
+    private fun setText(text: String){
+        textInputEditText.removeTextChangedListener(textWatcher)
+        textInputEditText.setTextCorrectly(text)
+        textInputEditText.addTextChangedListener(textWatcher)
+    }
+
+    fun TextInputEditText.setTextCorrectly(text: CharSequence){
+        setText(text)
+        setSelection(text.length)
     }
 }
