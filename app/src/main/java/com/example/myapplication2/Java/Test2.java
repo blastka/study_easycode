d83c660205e39f44525b5528afe887b649fe7166
blastka505 <necturna505@gmail.com>
2022-09-07 14:24:39 +0300

    тест с статичными методами

---
+++ app/src/main/java/com/example/myapplication2/Java/Test2.java
@@ -0,0 +1,10 @@
+package com.example.myapplication2.Java;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+
+        test1.firstMethod();
+        test1.secondMethod();
+    }
+}
