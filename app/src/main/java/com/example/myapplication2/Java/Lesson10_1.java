8269b911d8c194dbb3d8e23a62380f9574938122
blastka505 <necturna505@gmail.com>
2022-09-28 23:01:15 +0300

    урок 10

---
+++ app/src/main/java/com/example/myapplication2/Java/Lesson10_1.java
@@ -0,0 +1,25 @@
+package com.example.myapplication2.Java;
+
+public class Lesson10_1 {
+    public static void main(String[] args){
+        choice(20,100, 100);
+        choice(20,100, 120);
+        choice(20,100, 20);
+        choice(20,100, 0);
+    }
+
+    public static void choice(int beer, int whiskey, int money){
+        if (money >= (beer + whiskey)){
+            System.out.println("выпить пива и виски");
+        }
+        else if (money >= whiskey){
+            System.out.println("выпить виски");
+        }
+        else if (money >= beer){
+            System.out.println("выпить пива");
+        }
+        else{
+            System.out.println("уходим плакать домой");
+        }
+    }
+}
