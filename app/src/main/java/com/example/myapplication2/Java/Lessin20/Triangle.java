5eed8bbf3b0f3e759118b6e349ab0492217ff65b
blastka505 <necturna505@gmail.com>
2022-10-04 16:24:11 +0300

    Пример создания объекта в другом объекте

---
+++ app/src/main/java/com/example/myapplication2/Java/Lessin20/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.myapplication2.Java.Lessin20;
+
+public class Triangle {
+    private final int a;
+    private final int b;
+    private final int c;
+
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
