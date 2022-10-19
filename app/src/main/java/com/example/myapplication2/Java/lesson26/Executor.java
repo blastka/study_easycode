814cc29644061615d74a5b8a5f2e09a0cc9603b8
blastka505 <necturna505@gmail.com>
2022-10-19 15:44:05 +0300

    Комментарии, объясняющие что сделали. Типа имитация получения двух разных объектов через return но плохими способами

--- app/src/main/java/com/example/myapplication2/Java/lesson26/Executor.java
+++ app/src/main/java/com/example/myapplication2/Java/lesson26/Executor.java
@@ -3,7 +3,10 @@ package com.example.myapplication2.Java.lesson26;
 import androidx.annotation.Nullable;
 
 public class Executor {
-
+    /**
+     * Если пустой тип, как вернуть тогда эти два объекта?
+     * Сымитировали невозможность адекватного получения разных типов через return
+     */
     @Nullable
     public Object execute(){
         try{
--- app/src/main/java/com/example/myapplication2/Java/lesson26/Solution.java
+++ app/src/main/java/com/example/myapplication2/Java/lesson26/Solution.java
@@ -1,15 +1,16 @@
 package com.example.myapplication2.Java.lesson26;
 
-/**
- * В данный момент приведен код не ООП, нужно переделать
- */
+
 public class Solution {
     public static void main(String[] args) {
         Executor executor = new Executor();
         Object dataContainer = executor.execute();
+        /**
+         * Так был бы не ООП код, типа попытались решить проблему получения разных объектор
+         */
         if (dataContainer instanceof DataContainer){
             ((DataContainer) dataContainer).show();
         } else
-            ((ErrorInfo) dataContainer).log(new Log.Local());
+            ((ErrorInfo) dataContainer).log(new Log.Local());//тут порождаем объект, который внутри интерфейса
     }
 }
