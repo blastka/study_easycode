e12562591d420780b53fbaa832e7eb217df84709
blastka <svetuk505@mail.ru>
2022-10-31 21:51:10 +0300

    Более подробно про константы в классах и отдельно

--- app/src/main/java/com/example/myapplication2/Kotlin/lesson1/Test.kt
+++ app/src/main/java/com/example/myapplication2/Kotlin/lesson1/Test.kt
@@ -1,14 +1,25 @@
 package com.example.myapplication2.Kotlin.lesson1
 
 class Test {
-    //const val x = "x" // нельзя написать
+    //const val x = "x" // нельзя написать, нужно обернуть в объект
     companion object{
-        const val x = "x"
+        const val x = "x"//плохая константа ,похожа на статик
         private const val b = "b"
         //private const val c = UniqueList<String>()//нельзя, не примитив
     }
+
+    object H{
+        const val h = "r"
+    }
+
     fun main(){
         Test.b
         Test.x
+        R.r
     }
 }
+
+object R{
+    const val r = "r"
+}
+
